cb7e6936f4ae9af09bcab2227671b99495688b13
Talanova <pro_ego@mail.ru>
2021-10-26 18:59:15 +1000

    очистка инпутов

--- source/js/script.js
+++ source/js/script.js
@@ -1,3 +1,5 @@
+// Бургерное меню
+
 const mainNav = document.querySelector('.main-nav');
 const menuButtonOpen = document.querySelector('.main-nav__toggle');
 
@@ -5,5 +7,23 @@ menuButtonOpen.addEventListener('click', function () {
   mainNav.classList.toggle('main-nav--closed');
  });
 
+
+// Форма отзыва
+
+const form = document.querySelector('.review__form');
+const nameInput = form.querySelector('#name');
+const surnameInput = form.querySelector('#surname');
+const patronymicInput = form.querySelector('#patronymic');
+const numberInput = form.querySelector('#number');
+const emailInput = form.querySelector('#email');
+const reviewTextInput = form.querySelector('#review-text');
+const formButton = form.querySelector('.form__button');
+
+function clearInput (evt) {
+  evt.target.value = '';
+  evt.target.placeholder = '';
+}
+
+form.addEventListener('click', clearInput);
 
 
